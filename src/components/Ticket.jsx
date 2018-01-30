8688c05f48973213bd8b1d572f03244aa012d196
James Turello <jturello@yahoo.com>
2018-01-30 15:52:26 -0800

    pass props to Ticket component

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 function Ticket(props) {
   return(
     <div>
-      <h3>3a - Thato and Haley</h3>
-      <p><em>Firebase will not save record!</em></p>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
       <hr/>
     </div>
   );
